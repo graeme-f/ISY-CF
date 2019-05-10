c9fea501cb3f93a25989448149bd6be19478437d
thondariwin <thondariwin@10.50.0.33>
2019-05-10 10:41:13 +0630

    Added test data to consumables, paper data.

---
+++ Database/Consumables.data.sql
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Thondari Win
+ * Created: 02-May-2019
+ */
+//Paper//
+
+INSERT into Paper(Paper_ID, Start_Date, End_Date, Amount) 
+VALUES (1, "2017-01-01", "2017-02-01",  150),
+2, "2017-02-01", "2017-03-01",  175),
+3, "2017-03-01", "2017-04-01",  160),
+4, "2017-04-01", "2017-05-01",  175),
+5, "2017-05-01", "2017-06-01",  130),
+6, "2017-06-01", "2017-07-01",  145),
+7, "2017-07-01", "2017-08-01",  150),
+8, "2017-08-01", "2017-09-01",  175),
+9, "2017-09-01", "2017-010-01",  100),
+10, "2017-10-01", "2017-11-01",  125),
+11, "2017-11-01", "2017-12-01",  115),
+12, "2017-12-01", "2018-1-01",   135);
