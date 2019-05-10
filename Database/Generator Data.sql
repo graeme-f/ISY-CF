13c18daaebf799642fa3a88493464fef623d6ce8
SaiHtoo <SaiHtoo@10.50.0.36>
2019-05-10 12:03:08 +0630

    Created an insert into generator code

---
+++ Database/Generator Data.sql
@@ -0,0 +1,42 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2019 SaiHtoo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ * Author:  SaiHtoo
+ * Created: May 10, 2019
+ */
+
+insert into Generator (Start_Date, End_Date, Amount)
+Values ('2018-01-01' , '2018-01-31' , 631.333),
+       ('2018-02-01' , '2018-02-28' , 631.333),
+       ('2018-03-01' , '2018-03-31' , 631.333),
+       ('2018-04-01' , '2018-04-30' , 631.333),
+       ('2018-05-01' , '2018-05-31' , 631.333),
+       ('2018-06-01' , '2018-06-30' , 631.333),
+       ('2018-07-01' , '2018-07-31' , 631.333),
+       ('2018-08-01' , '2018-08-31' , 631.333),
+       ('2018-09-01' , '2018-09-30' , 631.333),
+       ('2018-10-01' , '2018-10-31' , 631.333),
+       ('2018-11-01' , '2018-11-30' , 631.333),
+       ('2018-12-01' , '2018-12-31' , 631.333);
+       
