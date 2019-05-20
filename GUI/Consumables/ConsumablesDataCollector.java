6e2e2d901539d0668bc2caf739fed980750e1fa8
mayankpandey <mayankpandey@10.50.0.52>
2019-05-20 11:00:06 +0630

    Added A3 and A4 columns plus methods too handle dates

--- GUI/Consumables/ConsumablesDataCollector.java
+++ GUI/Consumables/ConsumablesDataCollector.java
@@ -44,25 +44,29 @@ import javafx.stage.Stage;
 //Unsure whether the data type should be localdate or date, will require testing
 import java.time.LocalDate;
 import utility.DataCollector;
+import utility.DateUtil;
+import utility.ErrorMessage;
 
 /**
  *
  * @author mayankpandey
  */
 
-
 public class ConsumablesDataCollector extends DataCollector {
     
     //Made this class static to be usable in the SQL
     class Paper {
         int id;
-        int reams;
-        LocalDate Start_Date;
-        LocalDate End_Date;  
+        int A3reams;
+        int A4reams;
+        Date Start_Date;
+        Date End_Date;  
     }//end of paper class
     
     private static ConsumablesDataCollector singleInstance = null;
     HashMap <Integer, ConsumablesDataCollector.Paper> paperDetails;
+    
+    private Date lastDate;
     
     public static ConsumablesDataCollector getInstance() 
     { 
@@ -71,7 +75,6 @@ public class ConsumablesDataCollector extends DataCollector {
   
         return singleInstance; 
     } // end of getInstance method
-    
        
     // Creator is private to make this a singleton class
     private ConsumablesDataCollector(){
@@ -86,48 +89,46 @@ public class ConsumablesDataCollector extends DataCollector {
       String query = "SELECT * FROM Paper";
 
        ResultSet rs = doQuery(query);
-
       
       try {
       // iterate through the java resultset
       while (rs.next())
       {
-
-          //Creates an instance of the paper class, to be usable in this static method.
           Paper paper = new Paper();
         
-         //Find the tables with the same name located in the literal string and add them to paper's properties
+        //Find the tables with the same name located in the literal string and add them to paper's properties
         paper.id = rs.getInt("Paper_ID");
         Date startDate  = rs.getDate("Start_Date");
-        Date endDate = rs.getDate("date_created");
-        int reams = rs.getInt("Amount");
+        Date endDate = rs.getDate("End_Date");
+        int A3reams = rs.getInt("A3");
+        int A4reams = rs.getInt("A4");
         
         // Add that information into the hashmap
         paperDetails.put(paper.id, paper);
-      }//end of while loop
-      
-      
-      
+        
+        if (paper.End_Date == null || paper.End_Date.compareTo(lastDate) > 0){
+            lastDate = paper.End_Date;
+        }
+        
+      }//end of while loop 
       } //end of try statement
-   
-       catch (Exception e)
+     catch(SQLException error){
+            ErrorMessage.display(error.getMessage());
+     }
+     catch(Exception e) 
     {
       System.err.println("Returned SQL exception e");
       System.err.println(e.getMessage());
-    }//end of catch statement
-    
-        
+    }//end of try-catch codeblock    
     }//end of getAllPaper method
     
-    //gets the amount of reams for a given order
-    public int getReams(LocalDate startDate){
-        return paperDetails.get(startDate).reams;
-    } // end of method getReams
-
+      public java.util.Date getLastDate() {
+        return DateUtil.addDays(lastDate, 1);
+    } // end method getStartDate()
+    
     //gets the end date of a given order
-    public LocalDate getEndDate(LocalDate startDate){
+    public Date getEndDate(Date startDate){
         return paperDetails.get(startDate).End_Date;
     } // end of method getEndDate
-    
-    
+                      
 }//end of ConsumablesDataCollector class
