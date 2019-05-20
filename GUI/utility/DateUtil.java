3c2057020a8f86fc52df165e29e75fade2d04176
GFoster-ISY <gfoster@isyedu.org>
2019-05-20 11:29:24 +0630

    Change addDays to return a LocalDate

--- GUI/utility/DateUtil.java
+++ GUI/utility/DateUtil.java
@@ -1,15 +1,20 @@
 package utility;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil
 {
-    public static Date addDays(Date date, int days)
+    public static LocalDate addDays(Date date, int days)
     {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         cal.add(Calendar.DATE, days); //minus number would decrement the days
-        return cal.getTime();
+        Date d = cal.getTime();
+        Instant instant = d.toInstant();
+        return instant.atZone(ZoneId.systemDefault()).toLocalDate();
     }
 }
